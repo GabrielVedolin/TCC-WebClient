cc45277f7e24fcc1811939e3c7280a39dc489724
Gabriel Vedolin <gabrielvedolindonascimento63@gmail.com>
2022-08-15 21:56:32 -0300

    Removido Backend do repositorio

--- backend/src/models/Curso.js
+++
@@ -1,8 +0,0 @@
-import db from '../config/dbConnect.js';
-
- function ObterCursos(){
-    const cursos = db.query('select * from shae_db_local.cursos');
-    return cursos;
-}
-
-export default ObterCursos();
