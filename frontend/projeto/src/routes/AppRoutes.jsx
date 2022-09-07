c9aeec3e69d500935723a8ce9ef32a1164035153
Gabriel Plaza <gabriel.plaza@lansolver.com>
2022-09-07 17:55:58 -0300

    rotas privadas

--- frontend/projeto/src/routes/AppRoutes.jsx
+++ frontend/projeto/src/routes/AppRoutes.jsx
@@ -1,4 +1,5 @@
-import{
+import React, {useContext} from 'react';
+import {
     BrowserRouter as Router,
     Route,
     Routes,
@@ -6,24 +7,41 @@ import{
 } from "react-router-dom";
 import Quest from "../views/questionario1";
 import Login from "../views/login";
-import AlteraSenha from "../views/alterarSenha"; 
+import AlteraSenha from "../views/alterarSenha";
 import Feed from "../views/feed";
 import PageProf1 from "../views/pageProfessor1";
+import { AuthProvider, AuthContext } from "../contexts/auth";
+import { Children } from 'react';
 
-const AppRoutes = () =>{
-    return(
+
+
+const AppRoutes = () => {
+   
+const Private = ({children}) =>{
+    const { authenticated} = useContext(AuthContext);
+    if(!authenticated)
+    {
+        return <Navigate to="/" />;
+    }
+    
+        return children;
+    
+    };
+
+    return (
         <Router>
-        <Routes>
-            <Route exact path="/" element={<Login/>} />
-            <Route exact path="/quest" element={<Quest/>} />
-            <Route exact path="/alterarSenha" element={<AlteraSenha/>} />
-            <Route exact path="/feed" element={<Feed/>}/>
-            <Route exact path="/pageProf1" element={<PageProf1/>}/>
-            
-        </Routes>
+            <AuthProvider>
+                <Routes>
+                    <Route exact path="/" element={<Login />} />
+                    <Route exact path="/quest" element={<Private><Quest /></Private>} />
+                    <Route exact path="/alterarSenha" element={<Private><AlteraSenha /></Private>} />
+                    <Route exact path="/feed" element={<Private><Feed /></Private>} />
+                    <Route exact path="/pageProf1" element={<Private><PageProf1 /></Private>} />
+                </Routes>
+            </AuthProvider>
         </Router>
-      
-        );
+
+    );
 }
 
 export default AppRoutes;
--- frontend/projeto/src/views/login.js
+++ frontend/projeto/src/views/login.js
@@ -1,63 +1,68 @@
-import {useState } from "react";
+import { useState, useContext } from "react";
+import { AuthContext } from "../contexts/auth";
 import jpIMG from "../assets/icons-school.png";
 import "../styles/login.css"
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../App.css";
 
 
 function Login() {
+
   const navigate = useNavigate();
-  const handleSubmit = (e) =>{
-    e.preventDefault();
-    console.log(("submit", {email, password}));
-  }
+  const {authenticated, login} = useContext(AuthContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(("submit", { email, password }));
+    login(email, password);
+  }
   return (
     <div className="container">
       <div className="container-login">
         <div className="wrap-login">
-            <form className="login-form" onSubmit={handleSubmit}>
-              <span className="login-form-title">
-                <img src={jpIMG} alt="Jovem Programador" />
-              </span>
-              <span className="login-form-title"> Bem vindo </span>
+          <form className="login-form" onSubmit={handleSubmit}>
+            <span className="login-form-title">
+              <img src={jpIMG} alt="Jovem Programador" />
+            </span>
+            <span className="login-form-title"> Bem vindo </span>
+        <p>{String(authenticated)}</p>
+            <div className="wrap-input">
+              <input
+                className={email !== "" ? "has-val input" : "input"}
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
 
-              <div className="wrap-input">
-                <input
-                  className={email !== "" ? "has-val input" : "input"}
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />   
-                <span className="focus-input" data-placeholder="Email"></span>
-              </div>
+            <div className="wrap-input">
+              <input
+                className={password !== "" ? "has-val input" : "input"}
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <span className="focus-input" data-placeholder="Senha"></span>
+            </div>
 
-              <div className="wrap-input">
-                <input
-                  className={password !== "" ? "has-val input" : "input"}
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <span className="focus-input" data-placeholder="Senha"></span>
-              </div>
+            <div className="container-login-form-btn">
+              <button className="login-form-btn" type="submit" > Login </button>
+            </div>
 
-              <div className="container-login-form-btn">
-                <button className="login-form-btn" type="submit" > Login </button>
-              </div>
-
-              <div className="text-center">
-                <span className="txt1">Esqueceu a Senha? </span>
-                <a className="txt2" onClick={() => {navigate("/alterarSenha")}}>
-                  Alterar Senha
-                </a>
-              </div>
-            </form>       
+            <div className="text-center">
+              <span className="txt1">Esqueceu a Senha? </span>
+              <a className="txt2" onClick={() => { navigate("/alterarSenha") }}>
+                Alterar Senha
+              </a>
+            </div>
+          </form>
         </div>
       </div>
     </div>
