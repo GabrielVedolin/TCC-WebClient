ae41468fe2e843abd146bd99f0eea6ec2a6a6ce2
FelipeRDev <felipedx33@gmail.com>
2022-09-24 01:49:42 -0300

    Questionario agora retorna o JSON das repostas

--- frontend/projeto/src/views/questionario1.js
+++ frontend/projeto/src/views/questionario1.js
@@ -40,28 +40,35 @@ const steps = [
 export default function App() {
   const navigate = useNavigate();
   const [currentStep, setCurrentStep] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [formValues, setFormValues] = useState("");
+  let [loading, setLoading] = useState(false);
   const { logout } = useContext(AuthContext);
+  let arrayRespostas = [];
+  let arrayRespostas2 = [];
+  const [formValues, setFormValues] = useState({
+    quest1Field1: "",
+    quest1Field2: "",
+    quest1Field3: "",
+    quest1Field4: "",
+    quest2Field1: "",
+    quest2Field2: "",
+    quest2Field3: "",
+    quest2Field4: "",
+    quest2Field5: "",
+
+  });
+
+
   const handleLogout = () => {
     logout();
   };
 
   function handleNext() {
     setCurrentStep((prevState) => prevState + 1);
+ 
   }
 
   function handleBack() {
     setCurrentStep((prevState) => prevState - 1);
-  }
-
-  function handleInputChange(event) {
-    const { name, value } = event.target;
-
-    setFormValues((prevState) => ({
-      ...prevState,
-      [name]: value
-    }));
   }
 
   async function handleSubmit(e) {
@@ -74,6 +81,79 @@ export default function App() {
     // simulate api
     await fakeApi(() => setLoading(false), 2000);
   }
+
+  function nextAction (){
+    //console.log("array 1 dentro do next:" , arrayRespostas);
+    formValues.quest1Field1 = arrayRespostas[0];
+    formValues.quest1Field2 = arrayRespostas[1];
+    formValues.quest1Field3 = arrayRespostas[2];
+    formValues.quest1Field4 = arrayRespostas[3];
+
+    handleNext();
+  }
+
+  function readValues(){
+
+    //Repostas das questoes
+    var questao1 = document.getElementById("questao1");
+    var questao1Reposta = questao1.value;
+
+    var questao2 = document.getElementById("questao2");
+    var questao2Reposta = questao2.value;
+
+    var questao3 = document.getElementById("questao3");
+    var questao3Reposta = questao3.value;
+
+    var questao4 = document.getElementById("questao4");
+    var questao4Reposta = questao4.value;
+
+
+    arrayRespostas[0] = questao1Reposta;
+    arrayRespostas[1] = questao2Reposta;
+    arrayRespostas[2] = questao3Reposta;
+    arrayRespostas[3] = questao4Reposta;
+    
+  }
+  
+
+
+  function readValuesPage2(){
+
+    //Repostas das questoes page 2
+    var page2questao1 = document.getElementById("page2Questao1");
+    var page2Questao1Reposta = page2questao1.value;
+
+    var page2questao2 = document.getElementById("page2Questao2");
+    var page2Questao2Reposta = page2questao2.value;
+
+    var page2questao3 = document.getElementById("page2Questao3");
+    var page2Questao3Reposta = page2questao3.value;
+
+    var page2questao4 = document.getElementById("page2Questao4");
+    var page2Questao4Reposta = page2questao4.value;
+
+    var page2questao5 = document.getElementById("page2Questao5");
+    var page2Questao5Reposta = page2questao5.value;
+
+
+    arrayRespostas2[0] = page2Questao1Reposta;
+    arrayRespostas2[1] = page2Questao2Reposta;
+    arrayRespostas2[2] = page2Questao3Reposta;
+    arrayRespostas2[3] = page2Questao4Reposta;
+    arrayRespostas2[4] = page2Questao5Reposta;
+
+
+  }
+
+  function sendJson (){
+    formValues.quest2Field1 = arrayRespostas2[0];
+    formValues.quest2Field2 = arrayRespostas2[1];
+    formValues.quest2Field3 = arrayRespostas2[2];
+    formValues.quest2Field4 = arrayRespostas2[3];
+    formValues.quest2Field5 = arrayRespostas2[3];
+    //console.log("array respostas 2: ", arrayRespostas2);
+  }
+
 
   return (
     <div className="container">
@@ -105,86 +185,88 @@ export default function App() {
                 <div>
 
                   <p className="questionario-form-title"> Você assiste a séries regularmente?</p>
-                  <select className= "questionario-form-select">
-                    <option selected value="Selecione uma opção">Selecione uma opção</option>
-                    <option value="Sim, de 2 a 5 séries no ultimo ano">Sim, de 2 a 5 séries no ultimo ano</option>
-                    <option value="Sim, de 5 a 10 séries" >Sim, de 5 a 10 séries</option>
-                    <option value="Não assisto series regularmente" >Não assisto series regularmente</option>
+                  <select id="questao1" className="questionario-form-select" onChange= {readValues} >
+                  <option selected value={0}>Selecione uma opção</option>
+                    <option value={1}>Sim, de 2 a 5 séries no ultimo ano</option>
+                    <option  value={2}>Sim, de 5 a 10 séries</option>
+                    <option  value={3}>Não assisto series regularmente</option>
                   </select>
 
                   <p className="questionario-form-title"> Com que frequência você escuta música? </p>
-                  <select className= "questionario-form-select">
-                    <option selected value="Selecione uma opção" >Selecione uma opção"</option>
-                    <option value="1 a 5 vezes por semana" >1 a 5 vezes por semana</option>
-                    <option value="6 a 10 vezes na semana" >6 a 10 vezes na semana</option>
-                    <option value="Não estudo musica regularmente" >Não estudo musica regularmente</option>
+                  <select id="questao2" className="questionario-form-select" onChange= {readValues} >
+                    <option selected value= {0}>Selecione uma opção"</option>
+                    <option value={1}>1 a 5 vezes por semana</option>
+                    <option value={2}>6 a 10 vezes na semana</option>
+                    <option value={3} >Não estudo musica regularmente</option>
                   </select>
 
                   <p className="questionario-form-title"> Quantos livros leu nos últimos anos? </p>
-                  <select className= "questionario-form-select">
-                    <option  selected value="Selecione uma opção" >Selecione uma opção</option>
-                    <option value="2 a 5 livros no último ano" >2 a 5 livros no último ano</option>
-                    <option value="6 a 10 livros no último ano" >6 a 10 livros no último ano</option>
-                    <option value="Não li nenhum livro do último ano" >ão li nenhum livro do último ano</option>
+                  <select id="questao3" className="questionario-form-select" onChange= {readValues}>
+                    <option selected value= {0}>Selecione uma opção</option>
+                    <option value= {1}>2 a 5 livros no último ano</option>
+                    <option value= {2}>6 a 10 livros no último ano</option>
+                    <option value= {3}> Não li nenhum livro do último ano</option>
                   </select>
 
                   <p className="questionario-form-title"> Em questionarios, você tem mais facilidade com o que? </p>
-                  <select className= "questionario-form-select">
-                    <option selected value="Selecione uma opção" >Selecione uma opção</option>
-                    <option value="Multipla escolha" >Multipla escolha</option>
-                    <option value="Dissertativas" >Dissertativas</option>
-                    <option value="Ambos" >Ambos</option>
+                  <select  id="questao4" className="questionario-form-select" onChange= {readValues}>
+                    <option selected value= {0}>Selecione uma opção</option>
+                    <option value= {1}>Multipla escolha</option>
+                    <option value= {2}>Dissertativas</option>
+                    <option value= {3}>Ambos</option>
                   </select>
+
+                  
                 </div>
               )}
 
               {steps[currentStep].id === "Geral" && (
                 <div>
 
-                  <p className="questionario-form-title"> Quando você vai a um restaurente e vai escolher o seu prato, você:</p>
-                  <select className= "questionario-form-select">
-                    <option selected value="Selecione uma opção" >Selecione uma opção</option>
-                    <option value="Prefere ler o cardápio calmamente para escolher" >Prefere ler o cardápio calmamente para escolher</option>
-                    <option value="Prefere as opções de melhor aparência" >Prefere as opções de melhor aparência</option>
-                    <option value="Prefere que o garçom lhe explique alguma opção">Prefere que o garçom lhe explique alguma opção</option>
+                <p className="questionario-form-title"> Quando você vai a um restaurente e vai escolher o seu prato, você:</p>
+                  <select id="page2Questao1" className="questionario-form-select" onChange= {readValuesPage2}>
+                    <option selected value= {0}>Selecione uma opção</option>
+                    <option value= {1}>Prefere ler o cardápio calmamente para escolher</option>
+                    <option value= {2}>Prefere as opções de melhor aparência</option>
+                    <option value= {3}>Prefere que o garçom lhe explique alguma opção</option>
                   </select>
 
                   <p className="questionario-form-title"> Nos momentos livres você prefere: </p>
-                  <select className= "questionario-form-select">
-                    <option selected value="Selecione uma opção">Selecione uma opção</option>
-                    <option value="Ver TV" >Ver TV</option>
-                    <option value="Ler um livro" >Ler um livro</option>
-                    <option value="Escutar música" >Escutar música</option>
+                  <select  id="page2Questao2"className="questionario-form-select" onChange= {readValuesPage2}>
+                    <option selected value= {0}>Selecione uma opção</option>
+                    <option value= {1}>Ver TV</option>
+                    <option value= {2}>Ler um livro</option>
+                    <option value= {3}>Escutar música</option>
                   </select>
 
                   <p className="questionario-form-title"> Quando está vendo TV, você: </p>
-                  <select className= "questionario-form-select">
-                    <option selected value="Selecione uma opção">Selecione uma opção</option>
-                    <option value="Só consegue prestar atenção olhando para a tela" >Só consegue prestar atenção olhando para a tela</option>
-                    <option value="A imagem não tem tanta importância, servindo apenas como complementação." >A imagem não tem tanta importância, servindo apenas como complementação.</option>
+                  <select  id="page2Questao3"className="questionario-form-select" onChange= {readValuesPage2}>
+                    <option selected value= {0}>Selecione uma opção</option>
+                    <option value= {1}>Só consegue prestar atenção olhando para a tela</option>
+                    <option value= {2}>A imagem não tem tanta importância, servindo apenas como complementação.</option>
                   </select>
 
                   <p className="questionario-form-title"> Quando está estudando, você: </p>
-                  <select className= "questionario-form-select">
-                    <option selected value="Selecione uma opção">Selecione uma opção</option>
-                    <option value="Gosta de esquemas e mapas mentais" >Gosta de esquemas e mapas mentais</option>
-                    <option value="Gosta de ouvir ao invés de anotar" >Gosta de ouvir ao invés de anotar</option>
-                    <option value="Gosta de trabalhar com exemplos" >Gosta de trabalhar com exemplos</option>
+                  <select id="page2Questao4" className="questionario-form-select" onChange= {readValuesPage2}>
+                    <option selected value= {0}>Selecione uma opção</option>
+                    <option value= {1}>Gosta de esquemas e mapas mentais</option>
+                    <option value= {2}>Gosta de ouvir ao invés de anotar</option>
+                    <option value= {3}>Gosta de trabalhar com exemplos</option>
                   </select>
 
-                  <p className="questionario-form-title"> Quando vai estudar prefere: </p>
-                  <select className= "questionario-form-select">
-                    <option selected value="Selecione uma opção">Selecione uma opção</option>
-                    <option value="Livros que contenham esquemas e imagens" >Livros que contenham esquemas e imagens</option>
-                    <option value="Livros com questões e provas anteriores" >Livros com questões e provas anteriores</option>
-                    <option value="Prefere escutar uma aula e entender o que o professor fala">Prefere escutar uma aula e entender o que o professor fala</option>
+                  <p className="questionario-form-title" > Quando vai estudar prefere: </p>
+                  <select  id="page2Questao5"className="questionario-form-select" onChange= {readValuesPage2}>
+                    <option selected value= {0}>Selecione uma opção</option>
+                    <option value= {1}>Livros que contenham esquemas e imagens</option>
+                    <option value= {2}>Livros com questões e provas anteriores</option>
+                    <option value= {3}>Prefere escutar uma aula e entender o que o professor fala</option>
                   </select>
                 </div>
               )}
 
               {currentStep < steps.length - 1 && (
                 <div className="container-questionario-form-btn">
-                  <button className="questionario-form-btn" type="button" onClick={handleNext}>
+                  <button className="questionario-form-btn" type="button" onClick={nextAction}>
                     Next
                   </button>
                 </div>
@@ -192,7 +274,7 @@ export default function App() {
 
               {currentStep === steps.length - 1 && (
                 <div className="container-questionario-form-btn">
-                  <button className="questionario-form-btn" type="submit">
+                  <button className="questionario-form-btn" type="submit" onClick={sendJson}>
                     Enviar
                   </button>
                 </div>
